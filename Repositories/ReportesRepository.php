abdb07b4a581ea735b88329b45a72cc05c0d5a54
ivan886 <ivanfernando.fonseca@gmail.com>
2019-11-11 13:49:40 -0500

    purchase customers

--- Repositories/ReportesRepository.php
+++ Repositories/ReportesRepository.php
@@ -18,8 +18,27 @@ class ReportesRepository{
         $rows = $resource->fetchAll(PDO::FETCH_OBJ);
         return $rows;
     }
-
+    
+        public function getPurchaseCustomer() {
+        $sql = "    
+                select * 
+                from customers
+                inner join 
+                        (select sum(det.priceEach*det.quantityOrdered) purchase,cu.customerNumber
+                        from customers cu
+                        inner join orders ord
+                        on cu.customerNumber=ord.customerNumber
+                        inner join 	orderdetails det
+                        on ord.orderNumber=det.orderNumber
+                        group by cu.customerNumber
+                    ) purchase_customer
+                on  customers.customerNumber=purchase_customer.customerNumber
+                order by purchase_customer.purchase desc
+            ";
+        $conn= Connection::getConnection();
+        $resource = $conn->prepare($sql);
+        $resource->execute();
+        $rows = $resource->fetchAll(PDO::FETCH_OBJ);
+        return $rows;
+    }
 }
-
-$repo = new ReportesRepository();
-print_r($repo->getOrdersByCustomer(112));
