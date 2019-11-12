da3c7b8e35aee8d96b362d33e8606e6f5f5ae384
ivan886 <ivanfernando.fonseca@gmail.com>
2019-11-12 00:38:40 -0500

    autoloader

--- app/index.php
+++ app/index.php
@@ -1,9 +1,9 @@
 <?php
-
+/*https://thewebtier.com/php/psr4-autoloading-php-files-using-composer/*/
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../app/bootstrap.php';
-$data = App\Controller\ProductsController::find('S10_1949');
-print_r($data);
+//$data = App\Controller\ProductsController::find('S10_1949');
+//print_r($data);
